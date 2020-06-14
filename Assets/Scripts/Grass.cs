d5116e0ef3290cea5f4c96056d366bbc4e103b40
Jordan Orelli <jordanorelli@gmail.com>
2020-06-14 13:24:12 -0500

    grass eaten state

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -2,21 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grass : MonoBehaviour
-{
+public class Grass : MonoBehaviour {
+    public bool isEaten;
+    public Mesh uneatenMesh;
+    public Mesh eatenMesh;
+
+    private MeshFilter meshFilter;
+
     // Start is called before the first frame update
     void Start() {
-        
+        meshFilter = GetComponent<MeshFilter>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (isEaten) {
+            meshFilter.mesh = eatenMesh;
+        } else {
+            meshFilter.mesh = uneatenMesh;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
-        if (collider.CompareTag("Player")) {
-            Destroy(gameObject);
+        if (collider.CompareTag("Player") || collider.CompareTag("Enemy")) {
+            isEaten = true;
         }
     }
 }
